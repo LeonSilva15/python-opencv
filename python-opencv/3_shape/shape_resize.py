97a62b22527b8c16083cea5b47ed8a0e3258e346
Leon Silva <l30silva.o@gmail.com>
2020-07-30 23:18:33 -0500

    Added image resizing

---
+++ python-opencv/3_shape/shape_resize.py
@@ -0,0 +1,19 @@
+import cv2
+
+# Scaling
+# https://docs.opencv.org/4.2.0/da/d6e/tutorial_py_geometric_transformations.html
+
+# Read an image using cv2.imread
+img = cv2.imread('../../resources/lena.png')
+
+# The size of the image can be specified manually, or you can specify the scaling factor
+# Preferable interpolation methods are cv2.INTER_AREA for shrinking and cv2.INTER_CUBIC (slow)
+# and cv2.INTER_LINEAR for zooming
+img_scaled = cv2.resize(img, None, fx=2, fy=2, interpolation=cv2.INTER_CUBIC)
+cv2.imshow('Bigger Image', img_scaled)
+
+img_scaled = cv2.resize(img, None, fx=0.5, fy=0.5, interpolation=cv2.INTER_CUBIC)
+cv2.imshow('Smaller Image', img_scaled)
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
